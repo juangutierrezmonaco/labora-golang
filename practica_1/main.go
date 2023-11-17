1bbb4e0a43caf069e182566fc5cea1992cd267cb
Juan Manuel Gutierrez Monaco <juan.monaco.gutierrez@gmail.com>
2023-11-17 19:28:49 -0300

    modifying file structure to have utils for all exercises

--- practica_1/main.go
+++ practica_1/main.go
@@ -1,47 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"github.com/labora/labora-golang/practica_1/ejercicios"
-	"github.com/labora/labora-golang/practica_1/util"
+	"github.com/labora/labora-golang/util"
 )
 
-func validateAndSetOption(inf int, sup int) int {
-	var op int
-	fmt.Printf("Hola! Ingresá qué ejercicio querés correr (%d a %d, 0 para SALIR): ", inf+1, sup)
-	fmt.Scan(&op)
-	for op < inf || op > sup {
-		fmt.Printf("Opción incorrecta: Ingrese nuavemente (%d a %d, 0 para SALIR): ", inf+1, sup)
-		fmt.Scan(&op)
-	}
-	return op
-}
-
 func main() {
-	op := validateAndSetOption(0, 7)
-
-	if op != 0 {
-		functions := []func(){
-			practica.Ej1,
-			practica.Ej2,
-			practica.Ej3,
-			practica.Ej4,
-			practica.Ej5,
-			practica.Ej6,
-			practica.EjGrupo,
-		}
-
-		functions[op-1]()
-
-		var runAgain string
-		fmt.Print("\n¿Querés correr volver al INICIO? S/s: ")
-		fmt.Scan(&runAgain)
-		if runAgain == "S" || runAgain == "s" {
-			util.ClearConsole()
-			main()
-		} else {
-			fmt.Println("¡Adiós!")
-		}
+	functions := []func(){
+		practica.Ej1,
+		practica.Ej2,
+		practica.Ej3,
+		practica.Ej4,
+		practica.Ej5,
+		practica.Ej6,
+		practica.EjGrupo,
 	}
 
+	util.RunMenu(0, 7, functions)
 }
--- util/util.go
+++ util/util.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"runtime"
@@ -29,4 +30,35 @@ func ClearConsole() {
 	} else { //unsupported platform
 		panic("Your platform is unsupported! I can't clear terminal screen :(")
 	}
+}
+
+func validateAndSetOption(inf int, sup int) int {
+	var op int
+	fmt.Printf("Hola! Ingresá qué ejercicio querés correr (%d a %d, 0 para SALIR): ", inf+1, sup)
+	fmt.Scan(&op)
+	for op < inf || op > sup {
+		fmt.Printf("Opción incorrecta: Ingrese nuavemente (%d a %d, 0 para SALIR): ", inf+1, sup)
+		fmt.Scan(&op)
+	}
+	return op
+}
+
+func RunMenu(infOp int, supOp int, functions []func()) {
+	op := validateAndSetOption(infOp, supOp)
+
+	if op != 0 {
+
+		functions[op-1]()
+
+		var runAgain string
+		fmt.Print("\n¿Querés correr volver al INICIO? S/s: ")
+		fmt.Scan(&runAgain)
+		if runAgain == "S" || runAgain == "s" {
+			ClearConsole()
+			RunMenu(infOp, supOp, functions)
+		} else {
+			fmt.Println("¡Adiós!")
+		}
+	}
+
 }
