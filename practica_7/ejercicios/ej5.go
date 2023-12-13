66e9ebc6185992b5c498eccaf90f0ccacea164ac
Juan Manuel Gutierrez Monaco <juan.monaco.gutierrez@gmail.com>
2023-12-13 20:27:14 -0300

    exercises from 7th practice solved - 2b (added some text mods for printing)

--- practica_7/ejercicios/ej5.go
+++ practica_7/ejercicios/ej5.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 	_ "time"
+	"unicode"
 
 	"github.com/labora/labora-golang/util"
 )
@@ -234,16 +235,26 @@ func formatContenderName(c Contender) string {
 }
 
 func printTextSlowly(text string, milliseconds int) {
+	var wordCount int
 	for _, letter := range text {
 		fmt.Printf("%s", string(letter))
 		time.Sleep(time.Duration(milliseconds) * time.Millisecond)
+
+		// Prints an enter every 70 lines when it founds an space
+		if unicode.IsSpace(letter) {
+			wordCount++
+		}
+		if wordCount == 10 || string(letter) =="\t"{
+			fmt.Println()
+			wordCount=0
+		}
 	}
 	fmt.Printf("\n\n")
 }
 
 func printIntro(contenders []Contender) {
-	intro := "En NeoEclipsis, una ciudad sumida en la oscuridad y la corrupción, tres figuras emergen para enfrentarse en una lucha épica. Sir Luminis, el Paladín dedicado a la luz; el Teniente Vanguard, un policía implacable; y Sombra Nocturna, un criminal astuto.\n\nLa ciudad, una vez utópica, se tambalea al borde del colapso debido al crimen desenfrenado. Estos tres personajes, cada uno con su visión única de justicia, chocan en \"Sombras en Guerra\". ¿Prevalecerá la luz sobre la oscuridad o las sombras engullirán la ciudad? ¡Prepárate para una lucha que cambiará el destino de NeoEclipsis para siempre!"
-	printTextSlowly(intro, 10)
+	intro := "En NeoEclipsis, una ciudad sumida en la oscuridad y la corrupción, tres figuras emergen para enfrentarse en una lucha épica. Sir Luminis, el Paladín dedicado a la luz; el Teniente Vanguard, un policía implacable; y Sombra Nocturna, un criminal astuto.\n\tLa ciudad, una vez utópica, se tambalea al borde del colapso debido al crimen desenfrenado. Estos tres personajes, cada uno con su visión única de justicia, chocan en \"Sombras en Guerra\". ¿Prevalecerá la luz sobre la oscuridad o las sombras engullirán la ciudad? \t¡Prepárate para una lucha que cambiará el destino de NeoEclipsis para siempre!"
+	printTextSlowly(intro, 15)
 
 	fmt.Printf("¡Estos son nuestros valientes contendientes!, ¿quién ganará? ¡Sólo la SUERTE podrá decidirlo!\n\n")
 	for _, c := range contenders {
