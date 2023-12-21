78dfc7b1af4df25ff764078480322144bd60060f
Juan Gutierrez <juanmanuel@omniasalud.com>
2023-12-21 10:32:54 -0300

    pit fighters - printing intro

--- ejercicios_integratorios/fight_game/combat/combat.go
+++ ejercicios_integratorios/fight_game/combat/combat.go
@@ -156,7 +156,7 @@ func RunGame() {
 		contender.NewPaladin(),
 	}
 
-	// terminal.PrintIntro(contenders)
+	terminal.PrintIntro(contenders)
 	util.PressEnterToContinue()
 
 	noVictimSurvived := false
